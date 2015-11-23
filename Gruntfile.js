b904a031021c6d3ec65748231ce5d8862cb276cf
pirhoo <hello@pirhoo.com>
2015-11-23 18:19:16 +0100

    Upgrade imagemin

--- Gruntfile.js
+++ Gruntfile.js
@@ -396,6 +396,7 @@ module.exports = function (grunt) {
             'bower_components/**/*',
             'assets/images/{,*/}*.{webp}',
             'assets/fonts/**/*',
+            'assets/locales/*.json',
             'index.html'
           ]
         }, {
