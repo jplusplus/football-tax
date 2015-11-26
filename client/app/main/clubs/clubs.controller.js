5f5786bdfdabf97b654a7d20b51e15c7da7ba63c
pirhoo <hello@pirhoo.com>
2015-11-26 11:46:14 +0100

    Use relative year range

--- client/app/main/clubs/clubs.controller.js
+++ client/app/main/clubs/clubs.controller.js
@@ -4,5 +4,8 @@ angular.module('footballTaxApp')
   .controller('MainClubsCtrl', function ($scope, club) {
     $scope.club = club;
     $scope.payers = _.groupBy(club.transfers, 'payer');
-    $scope.years = _.range(2000, 2016);
+    $scope.years = _.range(
+      _.min(club.transfers, 'date').date * 1,
+      _.max(club.transfers, 'date').date * 1 + 1
+    );
   });
