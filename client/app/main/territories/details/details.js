5da7e34e876a9fb107fc90c38d1f677dd5fec1e7
pirhoo <hello@pirhoo.com>
2015-11-27 12:22:50 +0100

    Use territrory slug on frontend

--- client/app/main/territories/details/details.js
+++ client/app/main/territories/details/details.js
@@ -4,12 +4,12 @@ angular.module('footballTaxApp')
   .config(function ($stateProvider) {
     $stateProvider
       .state('main.territories.details', {
-        url: '/:id',
+        url: '/:slug',
         templateUrl: 'app/main/territories/details/details.html',
         controller: 'MainTerritoriesDetailsCtrl',
         resolve: {
           territory: function($stateParams, Restangular) {
-            return Restangular.one('territories', $stateParams.id).get()
+            return Restangular.one('territories', $stateParams.slug).get()
           }
         }
       });
