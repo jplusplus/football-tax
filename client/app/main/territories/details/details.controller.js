2741e3e2d77bc6d9762ae5691ae4ba8cb3a292b7
pirhoo <hello@pirhoo.com>
2015-11-27 13:10:28 +0100

    Completed territory details

--- client/app/main/territories/details/details.controller.js
+++ client/app/main/territories/details/details.controller.js
@@ -1,6 +1,32 @@
 'use strict';
 
 angular.module('footballTaxApp')
-  .controller('MainTerritoriesDetailsCtrl', function($scope, territory) {
+  .controller('MainTerritoriesDetailsCtrl', function($scope, currencies, territory, transfers) {
     $scope.territory = territory;
+    $scope.transfers = transfers;
+    $scope.years = _.range(
+      _.min(transfers, 'date').date * 1,
+      _.max(transfers, 'date').date * 1 + 1
+    );
+    // Pick the year with most spending
+    $scope.yearMostSpeding = _.chain(transfers)
+      // Clean currencies
+      .map( transfer=> {
+        transfer.value = currencies.fromStr(transfer.amount);
+        return transfer;
+      })
+      // Group transfers by date (year)
+      .groupBy('date')
+      // Create an array of objects for each year
+      .reduce( (res, transfers, date)=>{
+        res.push({
+          date: date,
+          transfers: transfers,
+          // Sum every transfer's value
+          total: _.reduce(transfers, (res, t)=>res + t.value, 0)
+        });
+        return res
+      }, [])
+      // Pick and return the year with the maximum value for 'total'
+      .max('total').value();
   });
--- client/app/main/territories/details/details.js
+++ client/app/main/territories/details/details.js
@@ -9,7 +9,18 @@ angular.module('footballTaxApp')
         controller: 'MainTerritoriesDetailsCtrl',
         resolve: {
           territory: function($stateParams, Restangular) {
-            return Restangular.one('territories', $stateParams.slug).get()
+            return Restangular.one('territories', $stateParams.slug).get();
+          },
+          transfers: function(territory) {
+            let all = _.chain(territory.clubs).reduce( (res, club)=>{
+              // Add the club namme to each transfers
+              _.map(club.transfers, t=> t.club = club.nameclub);
+              // Add the transfers array to the result
+              res.push(club.transfers);
+              return res;
+            }, []).value();
+            // Merge every transfers arrays
+            return _.union.apply(null, all);
           }
         }
       });
