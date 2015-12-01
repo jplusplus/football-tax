ca3316df5386ac884eec47760403ad165f545147
pirhoo <hello@pirhoo.com>
2015-12-01 15:07:04 +0100

    Automaticly add dterritories

--- server/config/seed.js
+++ server/config/seed.js
@@ -6,11 +6,14 @@
 'use strict';
 
 var Territory = require('../api/territory/territory.model');
+var clubs = require('../api/club/club.collection');
 
+var slug = require('slug'),
+       _ = require('lodash');
 
 Territory.find({}).remove(function() {
 
-  Territory.create({
+  let territories = [{
     name : 'Ville de Nice',
     country: 'FRA',
     slug: 'fra-ville-de-nice',
@@ -23,6 +26,27 @@ Territory.find({}).remove(function() {
     level: 0.2,
     // FeatureCollection containing one feature
     the_geom: require('../components/geojson/metropole-nice.json').features[0].geometry
-  });
+  }];
+
+  // Look into the club list
+  for(let club of clubs.toArray() ) {
+    // Each club has several transfers that may cocern a payer
+    let transfersByPayer = _.groupBy(club.getTransfers(), 'payer');
+    // Save every payer as a territory
+    for(let payerName in transfersByPayer) {
+      // Build territory slug using club's country
+      let territorySlug =  slug([club.country, payerName].join("-")).toLowerCase();
+      // A territory slug is uniq
+      if( _.any(territories, { slug: territorySlug })) continue;
+      // We will use a bulk function to insert all territories at once
+      territories.push({
+        name: payerName,
+        country: club.country.toUpperCase(),
+        slug: territorySlug
+      });
+    }
+  }
+
+  Territory.collection.insert(territories);
 
 });
