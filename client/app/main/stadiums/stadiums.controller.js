81930db06c9d5c9b214b68202b1791c21bedcc56
pirhoo <hello@pirhoo.com>
2015-12-03 14:20:12 +0100

    Added territory most spending

--- client/app/main/stadiums/stadiums.controller.js
+++ client/app/main/stadiums/stadiums.controller.js
@@ -8,13 +8,14 @@ angular.module('footballTaxApp')
       _.max(stadium.transfers, 'date').date * 1 + 1
     );
 
+    // Clean currencies
+    var transfers =  _.map(stadium.transfers, transfer=> {
+      transfer.value = currencies.fromStr(transfer.amount);
+      return transfer;
+    });
+
     // Pick the year with most spending
-    $scope.yearMostSpeding = _.chain(stadium.transfers)
-      // Clean currencies
-      .map( transfer=> {
-        transfer.value = currencies.fromStr(transfer.amount);
-        return transfer;
-      })
+    $scope.yearMostSpeding = _.chain(transfers)
       // Group transfers by date (year)
       .groupBy('date')
       // Create an array of objects for each year
@@ -28,5 +29,24 @@ angular.module('footballTaxApp')
         return res
       }, [])
       // Pick and return the year with the maximum value for 'total'
+      .max('total').value();
+
+    // Pick the entity with the transfers
+    $scope.territoryMostSpending = _.chain(transfers)
+      // Group transfers by payer
+      .groupBy('payer')
+      // Create an array of objects for each payer
+      .reduce( (res, transfers, payer)=>{
+        res.push({
+          payer: payer,
+          transfers: transfers,
+          // Sum every transfer's value
+          total: _.reduce(transfers, (res, t)=>res + t.value, 0),
+          // Find club from the first transfers (they are all the same)
+          club: transfers[0].club
+        });
+        return res
+      }, [])
+      // Pick and return the payer with the maximum value for 'total'
       .max('total').value();
   });
