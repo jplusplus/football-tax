be1e8f0b226069265448b220b09eac746a3946ca
pirhoo <hello@pirhoo.com>
2015-12-04 10:22:56 +0100

    Fixed currency figures

--- client/components/currencies/currencies.service.js
+++ client/components/currencies/currencies.service.js
@@ -6,12 +6,12 @@ angular.module('footballTaxApp')
     }
 
     Currencies.prototype.figuresFormat = function(d) {
-      if(d >= 10e9) {
-        return Math.round(d/10e9) + $translate.instant('BILLION_FIGURE');
-      } else if(d >= 10e6) {
-        return Math.round(d/10e6) + $translate.instant('MILLION_FIGURE');
-      } else if(d >= 10e3) {
-        return Math.round(d/10e3) + $translate.instant('THOUSAND_FIGURE');
+      if(d >= 1e9) {
+        return Math.round(d/1e9) + $translate.instant('BILLION_FIGURE');
+      } else if(d >= 1e6) {
+        return Math.round(d/1e6) + $translate.instant('MILLION_FIGURE');
+      } else if(d >= 1e3) {
+        return Math.round(d/1e3) + $translate.instant('THOUSAND_FIGURE');
       } else {
         return d;
       }
