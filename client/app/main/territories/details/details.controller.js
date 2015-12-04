7b22fd515f913d024920c5395af730b6caaf5d24
pirhoo <hello@pirhoo.com>
2015-12-04 10:33:37 +0100

    Updated data

--- client/app/main/territories/details/details.controller.js
+++ client/app/main/territories/details/details.controller.js
@@ -4,12 +4,12 @@ angular.module('footballTaxApp')
   .controller('MainTerritoriesDetailsCtrl', function($scope, currencies, compute, territory, transfers) {
     $scope.territory = territory;
     $scope.transfers = transfers;
-    $scope.years =  compute.years(transfers);  
+    $scope.years =  compute.years(transfers);
 
     // Clean currencies
     transfers = compute.cleanAmount(transfers);
     // Pick the year with most spending
     $scope.yearMostSpending = compute.mostSpending(transfers, 'date');
     // Pick the entity with the transfers
-    $scope.beneficiaryMostSpending = compute.mostSpending(transfers, 'beneficiary');
+    $scope.beneficiaryMostSpending = compute.mostSpending(transfers, 'beneficiary');    
   });
