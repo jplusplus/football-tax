b4a82e00a778fe351101085be3b9b9810bc53c8e
pirhoo <hello@pirhoo.com>
2016-01-12 16:55:55 +0100

    Added better languages support

--- client/app/app.translate.js
+++ client/app/app.translate.js
@@ -7,9 +7,25 @@ angular.module('footballTaxApp')
         prefix: 'assets/locales/',
         suffix: '.json'
       })
-      .registerAvailableLanguageKeys(['en', 'fr', 'de'])
-      .determinePreferredLanguage()
-      .fallbackLanguage(['en'])
+      .registerAvailableLanguageKeys(['en', 'fr', 'de'], {
+        'en_US': 'en',
+        'en_UK': 'en',
+        'fr_FR': 'fr',
+        'fr_CA': 'fr',
+        'fr_BE': 'fr',
+        'es_ES': 'es'
+      })
+      .determinePreferredLanguage(function() {
+        var lang = navigator.language || navigator.userLanguage;
+        console.log(lang)
+        var avalaibleKeys = [
+          'en_US', 'en_UK', 'en',
+          'fr_FR', 'fr_CA', 'fr_BE', 'fr',
+          'es_ES', 'es'
+        ];
+        return avalaibleKeys.indexOf(lang) === -1 ? 'en' : lang;
+      })
+      .fallbackLanguage(['en', 'fr'])
       .useMessageFormatInterpolation()
       .useCookieStorage()
       .useSanitizeValueStrategy(null);
