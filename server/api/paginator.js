ea5449f7b497e1d552e58e4cd756fa49cd4bbc89
pirhoo <hello@pirhoo.com>
2016-02-22 17:39:39 +0100

    More maximum item

--- server/api/paginator.js
+++ server/api/paginator.js
@@ -5,12 +5,12 @@ var config = require('../config/environment');
 module.exports.offset = function(req) {
   return {
     offset: Math.max(0,  req.query.offset || 0),
-    limit:  Math.min(20, req.query.limit  || 50)
+    limit:  Math.min(200, req.query.limit  || 50)
   };
 };
 
 module.exports.page = function(req) {
-  var limit = Math.min(50, req.query.limit  || 20);
+  var limit = Math.min(200, req.query.limit  || 50);
   return {
     page:   Math.max(1,  (req.query.offset/limit) + 1 || 1),
     limit:  limit
